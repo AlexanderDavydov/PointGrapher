0cc037790287e8b509ce697be6992ceb6433cfee
Alexander Davydov <davydov.al.d@gmail.com>
2025-06-23 10:26:00 +0300

    Update result screen and add landscape support

---
+++ app/src/main/java/com/example/pointgrapher/presentation/screen/result/ui/ResultScrollableTable.kt
@@ -0,0 +1,90 @@
+package com.example.pointgrapher.presentation.screen.result.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.displayCutoutPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.pointgrapher.presentation.screen.result.model.PointViewData
+
+@Composable
+internal fun ResultScrollableTable(
+    points: PointViewData,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        TwoColumnTableRow(colum1Text = "X", colum2Text = "Y")
+        LazyColumn(
+            content = {
+                items(points.x.size) {
+                    TwoColumnTableRow(
+                        colum1Text = "${points.x[it].toDouble()}",
+                        colum2Text = "${points.y[it]}",
+                        isPrimaryColor = it % 2 == 0
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun TwoColumnTableRow(
+    colum1Text: String,
+    colum2Text: String,
+    modifier: Modifier = Modifier,
+    isPrimaryColor: Boolean = false,
+) {
+    val rowAccentColor = if (isPrimaryColor) {
+        MaterialTheme.colorScheme.onPrimary
+    } else {
+        MaterialTheme.colorScheme.onSecondary
+    }
+
+    val rowAccentSecondaryColor = if (isPrimaryColor) {
+        MaterialTheme.colorScheme.onSecondary
+    } else {
+        MaterialTheme.colorScheme.onPrimary
+    }
+
+    Row(
+        modifier = modifier.background(rowAccentColor),
+        horizontalArrangement = Arrangement.Center,
+        content = {
+            Text(
+                modifier = Modifier
+                    .border(
+                        width = 1.dp,
+                        color = rowAccentSecondaryColor
+                    )
+                    .weight(0.5f)
+                    .fillMaxWidth(),
+                text = colum1Text,
+                textAlign = TextAlign.Center
+
+            )
+            Text(
+                modifier = Modifier
+                    .border(
+                        width = 1.dp,
+                        color = rowAccentSecondaryColor
+                    )
+                    .weight(0.5f)
+                    .fillMaxWidth(),
+                text = colum2Text,
+                textAlign = TextAlign.Center
+            )
+        })
+}
--- app/src/main/java/com/example/pointgrapher/presentation/screen/result/ui/ResultSuccessState.kt
+++ app/src/main/java/com/example/pointgrapher/presentation/screen/result/ui/ResultSuccessState.kt
@@ -1,16 +1,17 @@
 package com.example.pointgrapher.presentation.screen.result.ui
 
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.example.pointgrapher.presentation.screen.result.model.PointViewData
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
@@ -19,6 +20,7 @@ import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLineCartesianLayer
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
 import com.patrykandpatrick.vico.compose.cartesian.rememberVicoZoomState
+import com.patrykandpatrick.vico.core.cartesian.Zoom
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
@@ -29,42 +31,43 @@ internal fun ResultSuccessState(
     modifier: Modifier = Modifier,
     points: PointViewData
 ) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        content = {
-            ScrollableTable(
-                modifier = Modifier.weight(1f),
-                points = points
-            )
-            Graph(
-                modifier = Modifier.weight(1f),
-                points = points
-            )
-        },
-    )
-}
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
-
-@Composable
-private fun ScrollableTable(
-    points: PointViewData,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        modifier = modifier.horizontalScroll(rememberScrollState()),
-        content = {
-            // TODO use lazy column
-            Column {
-                points.x.forEachIndexed { index, point ->
-                    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                        Text("X: ${point.toDouble()}")
-                        Text("Y: ${points.y[index]}")
-                    }
-                }
+    if (isLandscape) {
+        Row(
+            modifier = modifier
+                .displayCutoutPadding()
+                .fillMaxSize(),
+            content = {
+                ResultScrollableTable(
+                    modifier = Modifier.weight(1f),
+                    points = points
+                )
+                Graph(
+                    modifier = Modifier.weight(1f),
+                    points = points
+                )
             }
-        }
-    )
+        )
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            content = {
+                ResultScrollableTable(
+                    modifier = Modifier.weight(1f),
+                    points = points
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Graph(
+                    modifier = Modifier.weight(1f),
+                    points = points
+                )
+            }
+        )
+    }
 }
+
 
 @Composable
 private fun Graph(
@@ -82,13 +85,13 @@ private fun Graph(
     }
 
     CartesianChartHost(
-        modifier = modifier,
+        modifier = modifier.fillMaxSize(),
         chart = rememberCartesianChart(
             rememberLineCartesianLayer(),
             startAxis = VerticalAxis.rememberStart(),
             bottomAxis = HorizontalAxis.rememberBottom(),
         ),
-        zoomState = rememberVicoZoomState(),
+        zoomState = rememberVicoZoomState(initialZoom = Zoom.Content),
         modelProducer = modelProducer,
     )
 }
