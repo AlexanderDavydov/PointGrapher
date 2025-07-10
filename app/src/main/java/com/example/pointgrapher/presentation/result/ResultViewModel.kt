0ed2b7aade6c23eeff9a15742c2028ee1be9e399
Alexander Davydov <davydov.al.d@gmail.com>
2025-07-10 15:58:49 +0300

    Fix warnings

--- app/src/main/java/com/example/pointgrapher/presentation/result/ResultViewModel.kt
+++ app/src/main/java/com/example/pointgrapher/presentation/result/ResultViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -40,6 +41,7 @@ class ResultViewModel @Inject constructor(
                 _state.value = ResultState.Success(points.mapToViewData())
             } catch (e: Exception) {
                 _state.value = ResultState.Error
+                Timber.e(e)
             }
         }
     }
